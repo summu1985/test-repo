7be7178e34fb72c93f4bce7a81d3e618415d981f
Sumit Mukherjee <summukhe@redhat.com>
2023-06-09 17:21:26 +0000

    <Any Commit Message>

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# dotnet-web-simple application
+
+It is a simple dotnet web application which uses `net7.0` as a target framework.
+
+ 
+# Application Output
+
+The app will print `Hello World!`
